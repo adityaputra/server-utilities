401c4a752c3b57866ad4c018f99b7ea9c6f2982a
Aditya Putra <me@adityaputra.com>
2021-10-07 14:10:26 +0800

    Create firewall.sh

---
+++ ddns-cloudflare-updater/firewall.sh
@@ -0,0 +1,14 @@
+# get current IP
+ip=$(curl https://ipinfo.io/ip)
+
+# update cloudflare firewall. refer to https://developers.cloudflare.com/firewall/api/cf-filters/put#update-a-single-filter
+curl -X PUT \
+     -H "Authorization: Bearer CF_TOKEN" \
+     -H "X-Auth-Key: REDACTED" \
+     -H "Content-Type: application/json" \
+     -d '{
+    "id": "CF_FIREWALL_FILTER_ID",
+    "paused": false,
+    "description": "allow-lab0-ip",
+    "expression": "(ip.src eq '$ip')"
+}' "https://api.cloudflare.com/client/v4/zones/CF_FIREWALL_ZONE_ID/filters/CF_FIREWALL_FILTER_ID"
